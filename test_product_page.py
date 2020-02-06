2babee73de26789003517f92d7385dd3257cdcb1
VladGPine <shikarno_3000@inbox.ru>
2020-02-06 12:45:34 +0300

    3 test cases added into test_product_page.py with xfail marks

--- test_product_page.py
+++ test_product_page.py
@@ -2,27 +2,51 @@ import pytest
 from pages.product_page import ProductPage
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                               marks=pytest.mark.xfail(reason='This promo link failed')),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+#                                                marks=pytest.mark.xfail(reason='This promo link failed')),
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+# def test_guest_can_add_product_to_basket(browser, link):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.adding_to_basket()
+#     page.solve_quiz_and_get_code()
+#     page.should_be_message_about_adding_product_to_basket()
+#     page.should_be_product_name_in_adding_product_to_basket_message_equal_product_name(
+#         page.return_product_name(),
+#         page.return_message_about_adding_product_to_basket())
+#     page.should_be_message_about_price_adding_product_to_basket()
+#     page.should_be_product_price_equal_in_basket_product_price(
+#         page.return_product_price(),
+#         page.return_product_price_in_basket())
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+@pytest.mark.xfail(reason='User does not see the success message about adding product to basket')
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.adding_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_message_about_adding_product_to_basket()
-    page.should_be_product_name_in_adding_product_to_basket_message_equal_product_name(
-        page.return_product_name(),
-        page.return_message_about_adding_product_to_basket())
-    page.should_be_message_about_price_adding_product_to_basket()
-    page.should_be_product_price_equal_in_basket_product_price(
-        page.return_product_price(),
-        page.return_product_price_in_basket())
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason='Message should disappear from the page')
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.adding_to_basket()
+    page.should_disappear_success_message()
