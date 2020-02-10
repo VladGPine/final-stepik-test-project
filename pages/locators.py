7b713925506952ef32993b481cee6e84ef2752e8
VladGPine <shikarno_3000@inbox.ru>
2020-02-10 15:09:34 +0300

    a few locators added into locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -7,11 +7,16 @@ class BasePageLocators:
     BASKET_LINK = (By.CSS_SELECTOR, '.btn-group > a.btn-default')
     PRODUCT_IN_BASKET = (By.CSS_SELECTOR, '.basket-items')
     EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, '#content_inner p')
+    USER_ICON = (By.CSS_SELECTOR, '.icon-user')
 
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    REGISTER_FORM_EMAIL_FIELD = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTER_FORM_PASSWORD_FIELD = (By.CSS_SELECTOR, '#id_registration-password1')
+    REGISTER_FORM_PASSWORD_FIELD_REPEAT = (By.CSS_SELECTOR, '#id_registration-password2')
+    REGISTER_FORM_SUBMIT_BUTTON = (By.CSS_SELECTOR, 'button[name="registration_submit"]')
 
 
 class ProductPageLocators:
