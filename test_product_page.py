9dc1ee584040abc30d13c926d987ab50997065d2
VladGPine <shikarno_3000@inbox.ru>
2020-02-10 15:15:47 +0300

    class TestUserAddToBasketFromProductPage added into test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 import pytest
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+import time
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -76,3 +78,34 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_present_any_products()
     basket_page.should_present_empty_basket_message()
+
+
+@pytest.mark.authorized_user
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        login_link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+        page = LoginPage(browser, login_link)
+        page.open()
+        page.register_new_user(email=str(time.time()) + '@fakemail.org', password='Q2345678!')
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.adding_to_basket()
+        page.should_be_message_about_adding_product_to_basket()
+        page.should_be_product_name_in_adding_product_to_basket_message_equal_product_name(
+            page.return_product_name(),
+            page.return_message_about_adding_product_to_basket())
+        page.should_be_message_about_price_adding_product_to_basket()
+        page.should_be_product_price_equal_in_basket_product_price(
+            page.return_product_price(),
+            page.return_product_price_in_basket())
